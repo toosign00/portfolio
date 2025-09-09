7780a71c541bc4e49683dffeae7fe7243c5b91b0
toosign <kevinsoo1014@gmail.com>
2025-09-10 03:10:18 +0900

    ♻️ refactor: React Query Key 상수로 관리

--- src/constants/queryKeys.constants.ts
+++ src/constants/queryKeys.constants.ts
@@ -4,4 +4,8 @@ export const QUERY_KEYS = {
     ALL: ['guestbook'] as const,
     DETAIL: (id: number) => ['guestbook', id] as const,
   },
+  PROJECTS: {
+    ALL: ['projects'] as const,
+    DETAIL: (id: string) => ['project', id] as const,
+  },
 } as const;
