b69e971107cbd7e198f0145e5f96366c82375033
toosign <kevinsoo1014@gmail.com>
2025-09-27 22:03:35 +0900

    🔧 fix: 이력서 다운로드 방식 개선 및 iOS Safari 호환성 추가 - 로컬 CV.pdf 파일을 Supabase storage로 전환 - iOS, iPadOS의 Safari 브라우저 감지 로직 추가로 다운로드 호환성 개선 - 다운로드 링크에 보안 속성(rel='noopener noreferrer') 추가 - 불필요한 로컬 PDF 파일 삭제

--- src/components/Intro/components/ProfileActions.tsx
+++ src/components/Intro/components/ProfileActions.tsx
@@ -1,6 +1,21 @@
 import { Button } from '@/components/Button';
+import { supabase } from '@/lib/supabase';
 
 export const ProfileActions = () => {
+  const { data } = supabase.storage.from('project').getPublicUrl('documents/CV.pdf', {
+    download: '노현수_이력서.pdf',
+  });
+
+  // IOS, iPadOS의 Safari 브라우저 확인
+  const isIOSSafari =
+    (/iPad|iPhone|iPod/.test(navigator.userAgent) ||
+      (navigator.userAgent.includes('Macintosh') && 'ontouchend' in document)) &&
+    navigator.userAgent.includes('Safari') &&
+    !navigator.userAgent.includes('Chrome') &&
+    !navigator.userAgent.includes('CriOS');
+  // console.log(isIOSSafari);
+  // console.log(navigator.userAgent);
+
   return (
     <div className='flex flex-row gap-4 sm:gap-6'>
       <Button
@@ -9,7 +24,11 @@ export const ProfileActions = () => {
         className='border-none md:rounded-lg md:px-4 md:py-2 md:text-base'
         variant='secondary'
       >
-        <a href='/assets/documents/CV.pdf' download='노현수_이력서.pdf'>
+        <a
+          href={data.publicUrl}
+          rel='noopener noreferrer'
+          target={isIOSSafari ? '_blank' : '_self'}
+        >
           이력서 다운로드
         </a>
       </Button>
